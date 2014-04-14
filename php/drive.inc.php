1bd40a3e28c580649b9878a2551092470a704383
Jake Breindel <j.breindel2@outlook.com>
2014-04-14 10:49:54 -0400

    Create drive.inc.php
    Shows a list of files from your Google Drive.

---
+++ php/drive.inc.php
@@ -0,0 +1,34 @@
+<h2 id="title">Drive Explorer</h2>
+	<table id="fileTable" class="list" border="0" cellspacing="1" cellpadding="2" width="940">
+    	<caption id="fileTableHeader">Showing Files</caption>
+    	<thead>
+        	<tr>
+	        	<th width="30">
+                	<a>Icon</a>
+                </th>
+	        	<th width="50">
+                	<a>Created</a>
+                </th>
+	        	<th width="280">
+                 	<a>Title</a>
+                </th>
+				<th width="180">
+                 	<a>Owner</a>
+                </th>
+                <th width="100">
+                    <a>Modified</a>
+                </th>
+        	</tr>
+     	</thead>
+     	<tbody id="fileTableBody">
+     	</tbody>
+    	<tfoot>
+     		<tr>
+        		<td id="footerContent" colspan="7">
+					Showing Files
+        		</td>
+     		</tr>
+    	</tfoot>
+    </table>
+	<input type="button" id="authorizeButton" class="btn_sm" style="float:right;margin: 10px 50px 20px auto;" value="Authorize" />
+	<div style="clear:both"></div>
